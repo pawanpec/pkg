669b026435b6d76f49040dd5a16852ed92592472
Pawan <pawanpec@gmail.com>
2015-03-08 14:21:53 +0530

    Review Codecomemet

--- spedia/src/main/java/com/spedia/controller/ReviewController.java
+++ spedia/src/main/java/com/spedia/controller/ReviewController.java
@@ -1,5 +1,6 @@
 package com.spedia.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -23,48 +24,68 @@ import com.spedia.service.review.IReviewService;
 public class ReviewController {
 	@Autowired
 	private IReviewService reviewService;
-	@RequestMapping(value="/get_all_unmoderated_review.html")
-	public ModelAndView reviewModerat(HttpServletRequest request, HttpServletResponse response) {
-		Map<String, Object> model=new HashMap<String, Object>();
+
+	@RequestMapping(value = "/get_all_unmoderated_review.html")
+	public ModelAndView reviewModerat(HttpServletRequest request,
+			HttpServletResponse response) {
+		Map<String, Object> model = new HashMap<String, Object>();
 		ModelAndView view = new ModelAndView("review_moderation");
-		List<Reviews> reviews=reviewService.getAllUnModeratedReviews();
-    	model.put("reviews", reviews);
-    	view.addAllObjects(model);
+		List<Reviews> reviews = reviewService.getAllUnModeratedReviews();
+		model.put("reviews", reviews);
+		view.addAllObjects(model);
 		return view;
-		
+
 	}
-	@RequestMapping(value="/approveReview.html")
-	public Reviews approveReview(HttpServletRequest request, HttpServletResponse response) {
-		Integer rid=Integer.parseInt(request.getParameter("rid"));
-		Reviews reviews=reviewService.approveReview(rid);
+
+	@RequestMapping(value = "/approveReview.html")
+	public Reviews approveReview(HttpServletRequest request,
+			HttpServletResponse response) {
+		Integer rid = Integer.parseInt(request.getParameter("rid"));
+		Reviews reviews = reviewService.approveReview(rid);
 		return reviews;
-		
+
 	}
-	@RequestMapping(value="/rejectReview.html")
-	public Reviews rejectReview(HttpServletRequest request, HttpServletResponse response) {
-		Integer rid=Integer.parseInt(request.getParameter("rid"));
-		Reviews reviews=reviewService.rejectReview(rid);
+
+	@RequestMapping(value = "/rejectReview.html")
+	public Reviews rejectReview(HttpServletRequest request,
+			HttpServletResponse response) {
+		Integer rid = Integer.parseInt(request.getParameter("rid"));
+		Reviews reviews = reviewService.rejectReview(rid);
 		return reviews;
-		
+
 	}
-	@RequestMapping(value="/writeReview.html")
-	public ModelAndView writeReview(HttpServletRequest request, HttpServletResponse response) {
-		ModelAndView view = new ModelAndView("writeReview","reviews", new Reviews());
+
+	@RequestMapping(value = "/writeReview.html")
+	public ModelAndView writeReview(HttpServletRequest request,
+			HttpServletResponse response) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		ModelAndView view = new ModelAndView("writeReview", "reviews",
+				new Reviews());
+		Map<Integer, String> ratingOption = new HashMap<Integer, String>();
+		ratingOption.put(1,"poor");
+		ratingOption.put(2,"Average");
+		ratingOption.put(3,"Good");
+		ratingOption.put(4,"very Good");
+		ratingOption.put(5,"Excellent");
+		model.put("ratingOption", ratingOption);
+		view.addAllObjects(model);
 		return view;
-		
+
 	}
+
 	@RequestMapping(value = "/submitReview.html")
-    public ModelAndView submit(@ModelAttribute("Reviews") Reviews reviews,BindingResult result) {
-    	ModelAndView view = new ModelAndView("writeReview");
-    	reviews.setStatus(0);
-    	Date date=new Date();
-    	Long created=date.getTime();
+	public ModelAndView submit(@ModelAttribute("Reviews") Reviews reviews,
+			BindingResult result) {
+		ModelAndView view = new ModelAndView("writeReview");
+		reviews.setStatus(0);
+		Date date = new Date();
+		Long created = date.getTime();
 		reviews.setCreated(created.intValue());
-    	reviewService.writeReview(reviews);
-    	 if (result.hasErrors()) {
-    	        return view;
-    	 }
+		reviewService.writeReview(reviews);
+		if (result.hasErrors()) {
+			return view;
+		}
 		return view;
-    }
+	}
 
 }
--- spedia/src/main/webapp/WEB-INF/pages/reviews/writeReview.jsp
+++ spedia/src/main/webapp/WEB-INF/pages/reviews/writeReview.jsp
@@ -1,19 +1,21 @@
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <html>
-    <head>
-    </head>
-    <body>
-        <h3>Welcome, Enter The Employee Details</h3>
-        <form:form method="POST" action="submitReview.html" modelAttribute="reviews">
-             <table>
-                <tr>
-                    <td><form:label path="review">Review TEXT</form:label></td>
-                    <td><form:input path="review"/></td>
-                </tr>
-                <tr>
-                    <td><input type="submit" value="Submit"/></td>
-                </tr>
-            </table>
-        </form:form>
-    </body>
+<head>
+</head>
+<body>
+	<h3>Welcome, Enter The Employee Details</h3>
+	<form:form method="POST" action="submitReview.html"
+		modelAttribute="reviews">
+				
+					<label for="edit-a">1. Academic Result </label>
+					<form:radiobuttons path="a" items="${ratingOption}" />
+					</br>
+					<label for="edit-a">1. Academic Result </label>
+					<form:radiobuttons path="a" items="${ratingOption}" />
+				
+					</br>
+				
+					<input type="submit" value="SubmitReview">
+	</form:form>
+</body>
 </html>
